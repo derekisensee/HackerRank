f9ee8b869494156078b4c7c5ece9617ce684a201
falldj <derek.isensee@gmail.com>
2017-02-13 22:17:58 +0000

    Add solutions for repeated coins of the same kind

--- src/CoinChange.java
+++ src/CoinChange.java
@@ -9,11 +9,11 @@ public class CoinChange {
     static int count;
 
     static void solve(Integer[] coins, int solveFor) {
-        for (Map.Entry m : solutions.entrySet()) { // check if we already have a solution
-            if (m.getKey().equals(coins)) {
-                // if above is true, we found a solution
-            }
-        }
+        // for (Map.Entry m : solutions.entrySet()) { // check if we already have a solution
+        //     if (m.getKey().equals(coins)) {
+        //         // if above is true, we found a solution
+        //     }
+        // }
 
         int sum = 0;
         for (int i = 0; i < coins.length; i++) {
@@ -23,14 +23,29 @@ public class CoinChange {
             count++;
         } else {
             // TODO: figure out what new "coins" array to create
-            
-            solve(coins, solveFor);
+            for (int i = 0; i < coins.length; i++) {
+		int aMod = solveFor % coins[i];
+		if (aMod == 0) {
+		    count++;
+		    Integer[] newCoins = new Integer[solveFor / coins[i]];
+		    for (int a = 0; a < solveFor / coins[i]; a++) {
+			newCoins[a] = coins[i];
+		    }
+		    solutions.put(newCoins, solveFor);
+		}
+		System.out.println("Finding: " + solveFor + " % " + coins[i] + " = " + (solveFor % coins[i]));
+	    }
+		    
+            //solve(coins, solveFor);
         }
     }
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(new File("coin.dat"));
 
+	solutions = new HashMap<>();
+	noCoins = new Integer[1];
+	noCoins[0] = 0;
         solutions.put(noCoins, 0);
 
         // read in amount, read in coins we have.
@@ -42,6 +57,16 @@ public class CoinChange {
         }
         // //
         solve(coins, n);
-        System.out.println(count);
+        // System.out.println(count);
+	// solutions.forEach((k,v) -> for (Integer s : k) {System.out.println("key: " + s + " value: " + v)});
+	for (Map.Entry<Integer[], Integer> e : solutions.entrySet()) {
+	    Integer[] key = e.getKey();
+	    Integer val = e.getValue();
+	    for (int i = 0; i < key.length; i++) {
+		System.out.print(key[i] + " " );
+	    }
+	    System.out.println(val);
+	}
+	
     }
 }
