2287e34bfa4eaf3873a6d5507d2d960bc63f5694
Derek Isensee <derek.isensee@gmail.com>
2017-02-14 08:28:12 -0600

    Add concurrent HashMap, still having problems.

--- src/CoinChange.java
+++ src/CoinChange.java
@@ -1,55 +1,76 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import java.io.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class CoinChange {
-    static HashMap<Integer[], Integer> solutions;
+    static ConcurrentHashMap<Integer[], Integer> solutions;
+    static HashMap<Integer[], Integer> tempSolutions;
     static Integer[] noCoins;
     static int count;
 
     static void solve(Integer[] coins, int solveFor) {
-        // for (Map.Entry m : solutions.entrySet()) { // check if we already have a solution
-        //     if (m.getKey().equals(coins)) {
-        //         // if above is true, we found a solution
-        //     }
-        // }
+//        for (Map.Entry<Integer[], Integer> entry : tempSolutions.entrySet()) {
+//            Integer[] key = entry.getKey();
+//            Integer value = entry.getValue();
+//            solutions.put(key, value);
+//            System.out.println("Stuck?");
+//        }
+        for (Map.Entry<Integer[], Integer> entry : solutions.entrySet()) {
+            Integer[] key = entry.getKey();
+            //Integer value = entry.getValue();
 
-        int sum = 0;
-        for (int i = 0; i < coins.length; i++) {
-            sum += coins[i];
+            if (!(Arrays.equals(key, coins))) {
+                int sum = 0;
+                for (int b = 0; b < coins.length; b++) {
+                    sum += coins[b];
+                }
+                solutions.put(coins, sum);
+                if (sum == solveFor) {
+                    count++;
+                } else {
+                    for (int i = 0; i < coins.length; i++) { // find repeating sum of a coin
+                        int aMod = solveFor % coins[i];
+                        if (aMod == 0) {
+                            System.out.println("Mod Tripped");
+                            count++;
+                            Integer[] newCoins = new Integer[solveFor / coins[i]];
+                            for (int a = 0; a < solveFor / coins[i]; a++) {
+                                newCoins[a] = coins[i];
+                            }
+                            solutions.put(newCoins, solveFor);
+                        }
+                        //System.out.println("Finding: " + solveFor + " % " + coins[i] + " = " + (solveFor % coins[i]));
+                    }
+                    // TODO: figure out what new "coins" array to create
+                    if (!(coins.length == 0)) {
+                        System.out.println("Tripped");
+                        Integer[] newCoins = new Integer[coins.length - 1];
+                        for (int a = 0; a < coins.length - 1; a++) {
+                            newCoins[a] = coins[a];
+                        }
+                        solve(newCoins, solveFor);
+                    }
+                }
+            }
         }
-	solutions.put(coins, sum);
-        if (sum == solveFor) {
-            count++;
-        } else {
-            for (int i = 0; i < coins.length; i++) { // find repeating sum of a coin
-		int aMod = solveFor % coins[i];
-		if (aMod == 0) {
-		    count++;
-		    Integer[] newCoins = new Integer[solveFor / coins[i]];
-		    for (int a = 0; a < solveFor / coins[i]; a++) {
-			newCoins[a] = coins[i];
-		    }
-		    solutions.put(newCoins, solveFor);
-		}
-		System.out.println("Finding: " + solveFor + " % " + coins[i] + " = " + (solveFor % coins[i]));
-	    }
-            // TODO: figure out what new "coins" array to create
-            for (int a = 0; a < coins.length; a++) {
-		for (int b = 0; b < coins.length; b++) {
-		    
-		}
-	    }
-        }
+//        for (Map.Entry<Integer[], Integer> entry : tempSolutions.entrySet()) {
+//            Integer[] key = entry.getKey();
+//            Integer value = entry.getValue();
+//            solutions.put(key, value);
+//        }
+
     }
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(new File("coin.dat"));
 
-	solutions = new HashMap<>();
-	noCoins = new Integer[1];
-	noCoins[0] = 0;
+        solutions = new ConcurrentHashMap<>();
+        tempSolutions = new HashMap<>();
+        noCoins = new Integer[1];
+        noCoins[0] = 0;
         solutions.put(noCoins, 0);
 
         // read in amount, read in coins we have.
@@ -62,15 +83,18 @@ public class CoinChange {
         // //
         solve(coins, n);
         // System.out.println(count);
-	// solutions.forEach((k,v) -> for (Integer s : k) {System.out.println("key: " + s + " value: " + v)});
-	for (Map.Entry<Integer[], Integer> e : solutions.entrySet()) { // messy and gross
-	    Integer[] key = e.getKey();
-	    Integer val = e.getValue();
-	    for (int i = 0; i < key.length; i++) {
-		System.out.print(key[i] + " " );
-	    }
-	    System.out.println(val);
-	}
+	    // solutions.forEach((k,v) -> for (Integer s : k) {System.out.println("key: " + s + " value: " + v)});
+        for (Map.Entry<Integer[], Integer> e : solutions.entrySet()) { // messy and gross
+            Integer[] key = e.getKey();
+            Integer val = e.getValue();
+            System.out.print("Array: ");
+            for (int i = 0; i < key.length; i++) {
+                System.out.print(key[i] + " " );
+            }
+            System.out.println("Sum: " + val);
+        }
+
+
 	
     }
 }
